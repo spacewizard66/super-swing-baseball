dbee5789319442d4ba6020e1a7a50f5875f9d5f7
spacewizard66 <mason4work@outlook.com>
2024-09-25 16:03:26 -0400

    Updated npm packages. Testing production.

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,12 +5,15 @@ import bg from '../../public/img/interior.jpg';
 
 export default function Intro(): any {
 
+    // Background image styles, stored in a variable.
+    // Necessary to import image for webpack processing.
     let background: React.CSSProperties = {
         "background": "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("+bg+")",
         "background-position": "center",
         "background-repeat": "no-repeat",
         "background-size": "cover"
     }
+
     return (
         <section
             className="intro"
