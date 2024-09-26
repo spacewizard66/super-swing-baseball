ecdea37cc489d9bfaadc70d00ab755a361a62c65
spacewizard66 <mason4work@outlook.com>
2024-09-26 03:06:35 -0400

    Replaced an npm package with a new, non deprecated one. node-sass --> sass. Fixed loading spinner image. Hoping this fixed build console errors.

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import spinner from '../../public/img/baseball.png';
+
 export default function LoadingSpinner(): React.JSX.Element {
     return (
         <section className="spinner">
             <img className="spinner__content"
                 alt=""
-                src="./img/favicon/baseball-96.png"
+                src={spinner}
             ></img>
             <h3>Loading...</h3>
         </section>
